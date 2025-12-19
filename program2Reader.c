9d09523503a3758ba9e8ff744bc43c9f5825a82a
ACTIVE NEBULA <137885439+vedansh-108@users.noreply.github.com>
2025-12-19 14:06:37 +0530

    Refactor program2Reader.c for code clarity

--- program2Reader.c
+++ program2Reader.c
@@ -1,43 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <string.h>
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+#include<sys/shm.h> 
+#include<string.h> 
 
-int main()
-{
-    int shmid;
-    void *shared_memory;
+int main() 
+{ 
+    int i; 
+    void *shared_memory; 
+    char buff[100]; 
+    int shmid; 
 
-    /* Get shared memory */
-    shmid = shmget((key_t)2345, 1024, 0666);
-    if (shmid == -1) {
-        perror("shmget failed");
-        exit(1);
-    }
+    shmid = shmget((key_t)2345, 1024, 0666); 
+    printf("Key of shared memory is %d\n", shmid); 
 
-    printf("Key of shared memory is %d\n", shmid);
-
-    /* Attach shared memory */
     shared_memory = shmat(shmid, NULL, 0);
-    if (shared_memory == (void *)-1) {
-        perror("shmat failed");
-        exit(1);
-    }
+    printf("Process attached at %p\n", shared_memory); 
 
-    printf("Process attached at %p\n", shared_memory);
-
-    /* Read data */
     printf("Data read from shared memory is : %s\n",
-           (char *)shared_memory);
-
-    /* Detach shared memory */
-    if (shmdt(shared_memory) == -1) {
-        perror("shmdt failed");
-        exit(1);
-    }
-
-    return 0;
+           (char *)shared_memory); 
 }
