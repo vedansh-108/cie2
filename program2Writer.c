e7ac54c0aad80fd737c23e426b852595129cae95
ACTIVE NEBULA <137885439+vedansh-108@users.noreply.github.com>
2025-12-19 14:02:56 +0530

    Refactor program2Writer.c for clarity and formatting
    Updated code formatting and removed redundant comments.

--- program2Writer.c
+++ program2Writer.c
@@ -1,57 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <string.h>
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+#include<sys/shm.h> 
+#include<string.h> 
 
-int main()
-{
-    int shmid;
-    void *shared_memory;
-    char buff[100];
-    ssize_t bytes_read;
+int main() 
+{ 
+    int i; 
+    void *shared_memory; 
+    char buff[100]; 
+    int shmid; 
 
-    /* Create shared memory */
-    shmid = shmget((key_t)2345, 1024, 0666 | IPC_CREAT);
-    if (shmid == -1) {
-        perror("shmget failed");
-        exit(1);
-    }
+    shmid = shmget((key_t)2345, 1024, 0666 | IPC_CREAT);   
+    printf("Key of shared memory is %d\n", shmid); 
 
-    printf("Key of shared memory is %d\n", shmid);
+    shared_memory = shmat(shmid, NULL, 0);  
+    printf("Process attached at %p\n", shared_memory);  
 
-    /* Attach shared memory */
-    shared_memory = shmat(shmid, NULL, 0);
-    if (shared_memory == (void *)-1) {
-        perror("shmat failed");
-        exit(1);
-    }
+    printf("Enter some data to write to shared memory\n"); 
+    read(0, buff, 100);  
 
-    printf("Process attached at %p\n", shared_memory);
-
-    printf("Enter some data to write to shared memory:\n");
-
-    /* Read input safely */
-    bytes_read = read(0, buff, sizeof(buff) - 1);
-    if (bytes_read == -1) {
-        perror("read failed");
-        exit(1);
-    }
-
-    buff[bytes_read] = '\0';  // Null-terminate input
-
-    /* Copy data to shared memory */
-    strcpy((char *)shared_memory, buff);
-
-    printf("You wrote : %s\n", (char *)shared_memory);
-
-    /* Detach shared memory */
-    if (shmdt(shared_memory) == -1) {
-        perror("shmdt failed");
-        exit(1);
-    }
-
-    return 0;
+    strcpy(shared_memory, buff); 
+    printf("You wrote : %s\n", (char *)shared_memory); 
 }
